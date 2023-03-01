edeaebe851618fa0ab8f8c8314d83badeb9b992e
sohilweb20 <sohailkhanhatewa@gmail.com>
2023-03-01 14:09:00 +0530

    create navbar

--- greektrust/src/Pages/HomePage.js
+++ greektrust/src/Pages/HomePage.js
@@ -1,7 +1,41 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "../Components/Navbar.css";
 const HomePage = () => {
-  return <div>HOMEPAGE</div>;
+  const [data, setData] = useState([]);
+  const getData = () => {
+    axios
+      .get(
+        `https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json`
+      )
+      .then((res) => setData(res.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  console.log("DATA", data);
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <div className="SearchInput">
+        <input type="text" placeholder="Search " />
+        <button>Search</button>
+      </div>
+      <div className="Clothes">
+        {data.length > 0 &&
+          data.map((item) => {
+            return (
+              <div>
+                <img style={{ width: "200px" }} src={item.imageURL} alt="" />
+                <h3px>{item.price}</h3px>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
